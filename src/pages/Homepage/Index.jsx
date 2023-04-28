c69c50a5a5bf29b7a83624b0ef59dcbcb17a3a8e
omar <omar.tourabi7@gmail.com>
2023-04-28 13:01:25 +0200

    ref: header component style

--- src/pages/Homepage/Index.jsx
+++ src/pages/Homepage/Index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Header } from '../../components/Header/Header'
 import './homepage.css'
+import { AsideBar } from '../../components/AsideBar/AsideBar'
 
 export const Homepage = () => {
     return (
         <>
             <Header />
-            <div>homepage</div>
+            <AsideBar />
         </>
     )
 }
