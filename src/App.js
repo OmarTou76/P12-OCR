7f85accf83f39dcae6e27cfce0157d9c0bc8fe31
omar <omar.tourabi7@gmail.com>
2023-04-28 11:48:32 +0200

    feat: homepage created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
+import { useEffect, useState } from "react";
+import { useFetch } from "./utils/useFetch";
+import { User } from "./models/User";
+import { Performance } from "./models/Performance";
+import { Activity } from "./models/Activity";
+import { AverageSessions } from "./models/AverageSessions";
+import { Homepage } from "./pages/Homepage/Index";
 
 function App() {
+  /* 
+    const [user, setUser] = useState({})
+    const [performance, setPerformance] = useState({})
+    const [activity, setActivity] = useState({})
+    const [average, setAaverage] = useState({})
+    const [userData, userLoading, userError] = useFetch(18)
+    const [perfData, perfLoading, perfError] = useFetch(18, "performance")
+    const [activityData, activityLoading, activityError] = useFetch(18, "activity")
+    const [averageData, averageLoading, averageError] = useFetch(18, "average-sessions")
+  
+    useEffect(() => {
+      if (userData && !userError && !userLoading) setUser(new User(userData.data))
+      if (perfData && !perfError && !perfLoading) setPerformance(new Performance(perfData))
+      if (activityData && !activityError && !activityLoading) setActivity(new Activity(activityData.data))
+      if (averageData && !averageError && !averageLoading) setAaverage(new AverageSessions(averageData.data))
+    }, [userData, userError, userLoading, perfData, perfError, perfLoading, activityData, activityError, activityLoading, averageError, averageData, averageLoading]) */
+
   return (
-    <div>
-      Init
-    </div>
+    <>
+      <Homepage />
+    </>
   );
 }
 
 export default App;
+
+{/* <div style={{
+      height: "100vh",
+      width: "100vw",
+      background: "black",
+      color: "white"
+    }}>
+      {userLoading ? <p>Loading</p> : (
+        <>
+          <p>{JSON.stringify(user)}</p>
+          <p>{JSON.stringify(performance)}</p>
+          <p>{JSON.stringify(activity)}</p>
+          <p>{JSON.stringify(average)}</p>
+        </>
+      )}
+    </div> */}
---
+++ src/pages/Homepage/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Header } from '../../components/Header/Header'
+import './homepage.css'
+
+export const Homepage = () => {
+    return (
+        <>
+            <Header />
+            <div>homepage</div>
+        </>
+    )
+}
