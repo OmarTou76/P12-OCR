2be165f23098da8d07a7fa6df9392805e89ce72e
omar <omar.tourabi7@gmail.com>
2023-05-03 13:49:11 +0200

    ref: User model can provide score

--- src/models/User.js
+++ src/models/User.js
@@ -1,18 +1,28 @@
 export class User {
+    model = {}
+    score = {}
     constructor(userData) {
-        this.model = {}
         this.createModel(userData)
-        return this.model
     }
 
     createModel(userData) {
+        const score = userData.todayScore || userData.score
         this.model = {
             id: userData.id,
             firstName: userData.userInfos.firstName,
             lastName: userData.userInfos.lastName,
             age: userData.userInfos.age,
-            score: userData.todayScore || userData.score,
+            score: score * 100,
             keyData: userData.keyData
         }
+        this.score = [
+            {
+                score: this.model.score,
+            },
+            {
+                score: Math.abs(this.model.score - 100) // rest of percent
+            },
+        ]
     }
+
 }
