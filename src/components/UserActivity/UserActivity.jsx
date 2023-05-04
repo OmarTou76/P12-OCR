23e8409d2511e9824e9b2e59999f3199876dd5d5
omar <omar.tourabi7@gmail.com>
2023-05-04 08:05:54 +0200

    ref: ratio of user activity

--- src/components/UserActivity/UserActivity.jsx
+++ src/components/UserActivity/UserActivity.jsx
@@ -30,8 +30,8 @@ export const UserActivity = ({ userId = 18 }) => {
         <div className='userActivity'>
             {!activity ? <p>...Loading</p> :
                 <ResponsiveContainer width="100%"
-                    aspect={4}>
-                    <BarChart barSize={12} data={activity}>
+                    aspect={3.5}>
+                    <BarChart barSize={9} data={activity}>
                         <Legend verticalAlign='top' height={60}
                             content={({ payload }) => {
                                 return (
