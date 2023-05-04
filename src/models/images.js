0c2925ebce5a789cd12759a22506147c149d6677
omar <omar.tourabi7@gmail.com>
2023-05-04 07:35:36 +0200

    feat: icon variables

---
+++ src/models/images.js
@@ -0,0 +1,11 @@
+import apple from '../assets/apple.svg'
+import chicken from '../assets/chicken.svg'
+import energy from '../assets/energy.svg'
+import cheeseburger from '../assets/cheeseburger.svg'
+
+export const ICON = {
+    protein: chicken,
+    calorie: energy,
+    carbohydrate: apple,
+    lipid: cheeseburger
+}
